7a2577485f99979d3f690abe12ec02cd0c4ca4e0
DaGamaCFO <Cfoofdagama@gmail.com>
2022-09-11 11:25:22 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RoboRunners
+
+Developed with Unreal Engine 4
---
+++ Source/RoboRunners/RoboRunnersGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RoboRunnersGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROBORUNNERS_API ARoboRunnersGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
